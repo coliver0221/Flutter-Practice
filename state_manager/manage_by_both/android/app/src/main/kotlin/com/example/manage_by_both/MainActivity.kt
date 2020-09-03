fd5e297147ec7e729408c41d50307390e33b5f46
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-09-03 15:04:59 +0800

    feat(state_manager/manage_by_both/): upload practice
    Practice of state manage by both widget and it's parent

---
+++ state_manager/manage_by_both/android/app/src/main/kotlin/com/example/manage_by_both/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.manage_by_both
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ state_manager/manage_by_both/lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ParentWidget(),
+    );
+  }
+}
+
+
+class ParentWidget extends StatefulWidget {
+  ParentWidget({Key key}) : super(key: key);
+
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void handleTapboxChange(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxC(active: _active, onChanged: handleTapboxChange),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged}) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _hightlight = false;
+
+  void handleTapDown(TapDownDetails details) {
+    setState(() {
+      _hightlight = true;
+    });
+  }
+
+  void handleTapUp(TapUpDetails details) {
+    setState(() {
+      _hightlight = false;
+    });
+  }
+
+  void handleTapCancel() {
+    setState(() {
+      _hightlight = false;
+    });
+  }
+
+  void handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: handleTapDown,
+      onTapUp: handleTapUp,
+      onTap: handleTap,
+      onTapCancel: handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(
+            widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _hightlight ? Border.all(color: Colors.teal[700], width: 5.0) : null,
+        ),
+      ),
+    );
+  }
+}
